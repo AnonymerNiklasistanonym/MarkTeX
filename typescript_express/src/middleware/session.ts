4fcbe0fe061f761aa64a87204eb7c9230489cc17
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-02-12 16:40:06 +0100

    Add debug configuration

--- typescript_express/src/middleware/session.ts
+++ typescript_express/src/middleware/session.ts
@@ -5,7 +5,7 @@ export const register = (app: any) => {
     app.use(session({
         resave: true,
         saveUninitialized: false,
-        secret: process.env.SESSION_SECRET
+        secret: process.env.SESSION_SECRET || 'secret'
     }));
 
     // TODO
