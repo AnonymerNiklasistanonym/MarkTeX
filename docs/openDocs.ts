024b474fa633ceea097abd4e251128b0292ac910
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-03-28 22:29:57 +0100

    Host documentation with seperate static express server
    This enables the user to use the search in browsers that block cors requests.
    Also no new dependency was necessary since the project already depends on express.

--- docs/openDocs.ts
+++ docs/openDocs.ts
@@ -1,8 +1,19 @@
 import { defaultDocsOutputDir } from "./createDocs";
+import express from "express";
 import { promises as fs } from "fs";
 import open from "open";
 import path from "path";
 
+const startStaticDocumentationServer = (port = 8081): Promise<string> => new Promise((resolve) => {
+    const app = express();
+    app.use(express.static(defaultDocsOutputDir));
+    app.listen(port, () => {
+        const url = `http://localhost:${port}`;
+        // eslint-disable-next-line no-console
+        console.log(`Server started at ${url}`);
+        resolve(url);
+    });
+});
 
 (async (): Promise<void> => {
     // Check if the documentation directory exists
@@ -10,14 +21,13 @@ import path from "path";
         await fs.access(defaultDocsOutputDir);
     } catch (err) {
         if (err.code === "ENOENT") {
-            // eslint-disable-next-line no-console
             console.error(`Error: Docs output directory was not found (${defaultDocsOutputDir}).`);
         } else {
-            // eslint-disable-next-line no-console
             console.error(err);
         }
         return process.exit(1);
     }
+    const url = await startStaticDocumentationServer();
     // Opens the main page of the documentation
-    await open(path.join(defaultDocsOutputDir, "index.html"));
+    await open(url);
 })();
