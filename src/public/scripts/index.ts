2639fad4aa447aca03eca6cb4047f075ecefa189
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-03-28 03:14:51 +0100

    Implement add new document to home page if logged in

--- src/public/scripts/index.ts
+++ src/public/scripts/index.ts
@@ -1,7 +1,9 @@
 
+import * as apiRequests from "./apiRequests";
+import * as notifications from "./notifications";
 import { keyboardListener, writeHelloWorld } from "./test";
 import { connectToSocket } from "./sockets";
-import handlebars from "handlebars";
+// import handlebars from "handlebars";
 
 
 const component = (content?: string): HTMLDivElement => {
@@ -25,24 +27,54 @@ window.addEventListener("load", (): void => {
 
     keyboardListener();
 
-    handlebars.registerHelper("customHelper", (input) => {
-        return `"${input}"`;
+    // handlebars.registerHelper("customHelper", (input) => {
+    //     return `"${input}"`;
+    // });
+    // window.alert("It works?");
+
+    // const template = "<div id=\"handlebars-template\">"
+    //     + "<div class=\"cat\">"
+    //     + "<h1>{{name}}</h1>"
+    //     + "<p>Age: {{age}}</p>"
+    //     + "<p>Helper: {{customHelper text}}</p>"
+    //     + "</div>"
+    //     + "</div>";
+    //
+    // const renderTemplate = handlebars.compile(template);
+    // document.body.appendChild(component(renderTemplate({
+    //     age: 42,
+    //     name: "haribo",
+    //     text: "This is nice"
+    // })));
+
+    const buttonRemove = document.getElementById("document-button-add") as HTMLButtonElement;
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    buttonRemove.addEventListener("click", async () => {
+        try {
+            const response = await apiRequests.document.create({
+                content: "",
+                date: new Date().toDateString(),
+                title: "New document"
+            });
+            if (response) {
+                await notifications.show({
+                    body: `New document was created ${response.title} by ${response.authors} from ${response.date}`,
+                    onClick: () => {
+                        const documentWindow = window.open(`/document/${response.id}`);
+                        if (documentWindow) { documentWindow.focus(); }
+                    },
+                    title: `Document was imported: ${response.title}`
+                });
+                // Redirect user to document page
+                window.location.href = `/document/${response.id}`;
+            } else {
+                // eslint-disable-next-line no-console
+                console.error("Something went wrong when creating a new document");
+            }
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            console.error(e);
+        }
     });
-    window.alert("It works?");
-
-    const template = "<div id=\"handlebars-template\">"
-        + "<div class=\"cat\">"
-        + "<h1>{{name}}</h1>"
-        + "<p>Age: {{age}}</p>"
-        + "<p>Helper: {{customHelper text}}</p>"
-        + "</div>"
-        + "</div>";
-
-    const renderTemplate = handlebars.compile(template);
-    document.body.appendChild(component(renderTemplate({
-        age: 42,
-        name: "haribo",
-        text: "This is nice"
-    })));
 
 });
